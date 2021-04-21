3d09674ffb74f45ff50025c6ecf7f2a1428a6182
Devin J. Pohly <djpohly@gmail.com>
2021-04-21 17:22:58 -0500

    assembler v3!
    Now uses Python 3, written a bit more Pythonically, simplifies label
    handling, and most importantly adds the .DATA directive!

--- assembler.py
+++ assembler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+from collections import defaultdict, namedtuple
 
 # Exception for when the IMM or REG functions find an inappropriate operand
 class OperandException(Exception):
@@ -9,242 +10,205 @@ class OperandException(Exception):
     def __str__(self):
         return self.msg
 
-regnames = {
-        'IP': 'R0',
-        'RP': 'R29',
-        'FP': 'R30',
-        'SP': 'R31',
+REG_NAMES = {
+        'ip': 'r0',
+        'rp': 'r29',
+        'fp': 'r30',
+        'sp': 'r31',
         }
-
 
 # convert a literal operand as it appears in the source file to
 # how it should appear in the intermediate representation.
 # If it's a number, then keep it as it is;
 # otherwise, if it's a valid identifier, assume it's a label (including register names)
-def IMM(operand):
+def IMM(operand, ofs, relocs):
     try:
-        return str(int(operand))
+        return [int(operand)]
     except ValueError:
         pass
     if not operand.isidentifier():
-        raise OperandException("Needs literal or label, found '%s'" % operand)
-    return '#' + operand
+        raise OperandException(f"expected literal or label, found '{operand}'")
+    relocs[operand].append(ofs)
+    return [0]
 
 # convert a register operand as it appears in the source file
 # to how it should appear in the intermediate representation.
 # Specifically, it better begin with R and then have a number from 0 to 32.
 # If not, raise an appropriate exception
-def REG(operand):
+def REG(operand, ofs, relocs):
     try:
-        operand = regnames[operand.upper()]
+        operand = REG_NAMES[operand.casefold()]
     except KeyError:
         pass
-    if operand[0] != 'R' or not operand[1:].isdigit():
-        raise OperandException("Needs register, found %s" % operand)
+    if operand[0].casefold() != 'r' or not operand[1:].isdigit():
+        raise OperandException(f"expected register, found '{operand}'")
     elif int(operand[1:]) not in range(32):
-        raise OperandException("%s is not a valid register" % operand)
+        raise OperandException(f"invalid register '{operand}'")
     else:
-        return operand[1:]
+        return [operand[1:]]
+
+def DATA(operand, ofs, relocs):
+    try:
+        data_bytes = int(operand)
+    except ValueError:
+        data_bytes = -1
+    if data_bytes < 0:
+        raise OperandException(f"expected nonnegative integer, found '{operand}'")
+    return [0] * data_bytes
 
 
 # map from instruction mnemonics to lists describing the format of that
 # instruction (numeric code + functions to convert/check the operands)
-instructions = {
-        'MOVI': [1, IMM, REG],
-        'MOV': [2, REG, REG],
-        'ADD': [3, REG, REG],
-        'SUB': [4, REG, REG],
-        'MUL': [5, REG, REG],
-        'IDIV': [6, REG, REG],
-        'JMP': [7, REG],
-        'JNZ': [8, REG, REG],
-        'OUT': [9, REG],
-        'HALT': [10],
-        'LD': [11, REG, REG],
-        'ST': [12, REG, REG],
-        'JAL': [13, REG],
-        'RET': [14],
-        'PUSH': [15, REG],
-        'POP': [16, REG],
-        'LDLO': [17, IMM, REG],
-        'STLO': [18, IMM, REG],
+INSNS = {
+        'movi': [[1], IMM, REG],
+        'mov': [[2], REG, REG],
+        'add': [[3], REG, REG],
+        'sub': [[4], REG, REG],
+        'mul': [[5], REG, REG],
+        'idiv': [[6], REG, REG],
+        'jmp': [[7], REG],
+        'jnz': [[8], REG, REG],
+        'out': [[9], REG],
+        'halt': [[10]],
+        'ld': [[11], REG, REG],
+        'st': [[12], REG, REG],
+        'jal': [[13], REG],
+        'ret': [[14]],
+        'push': [[15], REG],
+        'pop': [[16], REG],
+        'ldlo': [[17], IMM, REG],
+        'stlo': [[18], IMM, REG],
+        '.data': [[], DATA],
         }
 
 
 # check to make sure there's a filename
 if len(sys.argv) < 2:
-    print("Usage: ./assembler filename.asm")
+    print(f"Usage: {sys.argv[0]} filename.asm")
     sys.exit(1)
 
-# try to open the file
+# dump the file into a list (allowing us to refer back to lines for error reporting)
+filename = sys.argv[1]
 try:
-    asm_file = open(sys.argv[1])
+    with open(filename) as asm_file:
+        lines = list(asm_file)
 except IOError:
-    print("Can't find file %s (or some similar problem)" % sys.argv[1])
+    print(f"{sys.argv[0]}: error: unable to find or open file '{filename}'")
+    sys.exit(1)
+except UnicodeDecodeError:
+    print(f"{sys.argv[0]}: error: file '{filename}' does not appear to be a text file")
     sys.exit(1)
 
 
-# dump the file into a list (so that I can refer back to lines for error reporting)
-lines = []
-for line in asm_file:
-    lines.append(line)
+# First pass: convert to machine language with unresolved labels
 
+# Map from label names to (offset, line number) tuple
+Label = namedtuple('Label', ['ofs', 'line'])
+all_labels = {}
 
-# ******* first pass: make a list of instructions and map of labels, and flag
-# any unknown instructions or instructions used with wrong operands
+# Map from label names to a list of offsets which should contain the address
+# of that label
+relocs = defaultdict(list)
 
-# if there's an error, I don't want to quit right away (so that other
-# errors can be reported on the same run), but I do want to quit
-# at the end of the pass
+# Compiled machine language
+compiled = []
+
+# Don't quit first pass immediately on error; instead, remember whether there
+# was an error and exit before second pass
 error = False
 
-# list of "intermediate code" instructions, each a list
-instr_seq = []
+def warn(lnr, msg, line=None):
+    global filename, lines
+    print(f"{filename}: warning: {msg}:")
+    print(f" {lnr:3}: " + lines[lnr - 1].rstrip('\r\n'))
 
-# map labels to the bytes where they are defined (which thus
-# also is the label's value)
-lab_def_byte = {}
+def err(lnr, msg, line=None):
+    global filename, lines, error
+    print(f"{filename}: error: {msg}:")
+    print(f" {lnr:3}: " + lines[lnr - 1].rstrip('\r\n'))
+    error = True
 
-# map labels to the lines where they are defined (for error reporting)
-lab_def_line = {}
+for lnr, orig_line in enumerate(lines, 1):
+    orig_line = orig_line.rstrip('\r\n')
 
-# map labels to the first line in which they are used (for error reporting)
-lab_use_line = {}
+    # Split off anything after // as a comment
+    line, _, comment = map(str.strip, orig_line.partition('//'))
 
-# how many bytes have we processed?
-byte_num = 0
+    # Process any label(s) found at the beginning of the line
+    labels, _, line = map(str.strip, line.rpartition(':'))
+    if labels:
+        # Store offset and line number of label
+        newlbl = Label(len(compiled), lnr)
 
-# which line are we on?
-line_num = 1
+        # Associate this label with all label names on the line
+        for lblname in map(str.strip, labels.split(':')):
+            # Ignore "labels" which are probably offsets from disassembler
+            if lblname.isdigit():
+                continue
 
-for line in lines:
-    # get rid of comments and spaces
-    comment_begin = line.find('//')
-    if comment_begin != -1:
-        working_line = line[:comment_begin]
-    else:
-        working_line = line
-    working_line = working_line.strip().upper()
+            # Require that labels be valid identifiers
+            if not lblname.isidentifier():
+                err(lnr, f"invalid label name '{lblname}'")
+                continue
 
-    #grab any labels
-    while True:
-        lab_end = working_line.find(':')
-        if lab_end == -1:
+            # Ensure that each label is defined in only one place
+            label = all_labels.get(lblname)
+            if label:
+                err(lnr, f"label '{lblname}' previously defined at {filename}:{label.line}")
+                continue
+
+            # Store the label
+            all_labels[lblname] = newlbl
+
+    try:
+        op, *operands = line.split()
+    except ValueError:
+        # Nothing here but whitespace
+        continue
+
+    try:
+        opcode, *fmts = INSNS[op.casefold()]
+    except KeyError:
+        err(lnr, f"unknown instruction '{op}'")
+        continue
+
+    if len(operands) != len(fmts):
+        err(lnr, f"wrong number of operands to '{op}' (expected {len(fmts)}, got {len(operands)})")
+        continue
+
+    compiled += opcode
+    for opnum, fmt_fn in enumerate(fmts):
+        try:
+            compiled += fmt_fn(operands[opnum], len(compiled), relocs)
+        except OperandException as e:
+            err(lnr, f"operand {opnum + 1}: {e.msg}")
             break
-        label = working_line[:lab_end]
-        if label.isdigit():
-            # Probably an offset, ignore
-            pass
-        elif not label.isidentifier():
-            print("Bad label %s in line %s" %(label, str(line_num)))
-            print(line)
-            error = True
-        elif label in lab_def_byte:
-            assert label in lab_def_line
-            print("Label %s previously defined on line %s" % (label, lab_def_line[label]))
-            print(line)
-            error = True
-        else:
-            lab_def_byte[label] = byte_num
-            lab_def_line[label] = line_num
-        working_line = working_line[lab_end+1:].strip()
 
-    #read the instruction, if any
-    instr_pieces = working_line.split()
-    if len(instr_pieces) > 0:
-        op_code = instr_pieces[0].upper()  # the mnemonic code for the instruction
-        # is it a valid instruction?
-        if op_code not in instructions:
-            print("Unknown instruction %s in line %s" %(instr_pieces[0], str(line_num)))
-            print(line)
-            error = True
+for labelname, label in all_labels.items():
+    if labelname not in relocs:
+        warn(label.line, f"unused label '{labelname}'")
 
-        else :  # ok, it's a real instruction
-            instr_format = instructions[op_code]
-            # does it have the right number of operands?
-            if len(instr_format) != len(instr_pieces):
-                print("Wrong number of operands to %s instruction in line %s" % (op_code, str(line_num)))
-                print("Needs %s, found %s" % (str(len(instr_format) - 1),
-                        str(len(instr_pieces) - 1)))
-                print(line)
-                error = True
-            else: # ok, it has the right number of operands
-                # the "intermediate code" for this instruction
-                instr_byte_seq = [str(instr_format[0])]
-                # check each operand, adding it to the intermediate code for this
-                # instruction if ok
-                for i in range(1, len(instr_format)):
-                    try:
-                        next_byte = instr_format[i](instr_pieces[i])
-                        if next_byte[0] == '#' and next_byte[1:] not in lab_use_line:
-                            lab_use_line[next_byte[1:]] = line_num
-                        instr_byte_seq.append(instr_format[i](instr_pieces[i]))
-                    except OperandException as e:
-                        print("Bad operand %s in line %s" % (str(i), str(line_num)))
-                        print(e.msg)
-                        print(line)
-                        error = True
-                # increment our byte count by the number of instructions
-                byte_num += len(instr_format)
-                # add this instruction to our intermediate code list
-                instr_seq.append(instr_byte_seq)
-    # increment the line counter no matter what (even if the line is blank),
-    # since this is for debugging
-    line_num += 1
-
-# check to see that all defined labels are used somewhere
-for label in lab_def_line:
-    if label not in lab_use_line:
-        print("Warning: label %s unused" % label)
-
-# don't do a second pass if there was an error on the first
 if error:
-    sys.exit(1)
+    exit(1)
 
-# ****** Second pass: go over the intermediate code, populating
-# a list with the target code (the only reason we don't just write
-# to a file here is because we're still checking for errors)
+# Resolve labels
+for lblname, offsets in relocs.items():
+    try:
+        lblofs = all_labels[lblname].ofs
+    except KeyError:
+        err(lnr, f"undefined label '{lblname}'")
+        continue
+    for ofs in offsets:
+        compiled[ofs] = lblofs
 
-# the target code
-instr_bytes = []
-# the list of labels we've already produced "undefined" errors for;
-# we want to generate an error only for the first use of an undefined label
-bad_labels = []
-
-for instr in instr_seq:
-    for x in instr:
-        # is it a label?
-        if x[0] == '#':
-            # strip off the #
-            lab = x[1:]
-            # is it defined somewhere?
-            if lab not in lab_def_byte and lab not in bad_labels:
-                print("Undefined label %s used on line %d:" % (lab, lab_use_line[lab]))
-                print("   " + lines[lab_use_line[lab] - 1])
-                error = True
-                bad_labels.append(lab)
-            else:
-                # ok, it's properly defined, emit its value
-                instr_bytes.append(str(lab_def_byte[lab]))
-        else:
-            # not a label; just emit it as is
-            instr_bytes.append(x)
-
-# if something bad happened (that is, an undefined label),
-# then don't write to the file
 if error:
-    sys.exit(1)
+    exit(1)
 
 # if the source file ends with .asm, then replace it with .vml, otherwise
 # just add .vml
-src_file_name = sys.argv[1]
-if src_file_name[-4:] == '.asm':
-    src_file_name = src_file_name[:-4]
-targ_file_name = src_file_name + '.vml'
+out_filename = filename.removesuffix('.asm') + '.vml'
 
 # write the program
-f = open(targ_file_name, 'w')
-f.write(str(len(instr_bytes)) + '\n')
-for x in instr_bytes:
-    f.write(x + '\n')
-f.close()
+with open(out_filename, 'w') as f:
+    print(len(compiled), file=f)
+    print("\n".join(map(str, compiled)), file=f)
