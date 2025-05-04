71a945d371968c4a88bcdb2527a87805fe493262
j0shjoseph <122111374+j0shjoseph@users.noreply.github.com>
2025-05-03 21:32:57 -0500

    final work

--- gcd.asm
+++ gcd.asm
@@ -1,84 +1,61 @@
-
+MOVI 100 R31
+MOV  R31 R30
 MOVI 180 R0
-MOVI 18 R1     
-MOVI 24 R2     
+MOVI 18  R1
+MOVI start R8
+MOVI end R9
+MOVI process R10
+MOVI gcd R12
+MOVI rec R13
+MOVI return R14
 
-MOVI loop R10        
-MOVI end R11         
-MOVI loop_body R12   
-MOVI gcd R13         
-MOVI recurse R14     
-MOVI return R15      
-MOVI mod_loop R9 
-MOVI mod_loop R20
-MOVI mod_continue R21
-MOVI mod_done R22
+start:
+    MOV  R1     R15
+    MOVI 25     R6
+    SUB  R15    R6
+    JNZ  R16    R10
+    JMP  R9
 
-loop:
-    MOV R1 R7   
-    SUB R2 R7     
-    JNZ R7 R12 
-    JMP R11
-    
-loop_body:
-    MOV R0 R3
-    MOV R1 R4
-
+process:
+    MOV  R0     R3
+    MOV  R1     R4
     PUSH R3
     PUSH R4
-    JAL R13
-    POP R4
-    POP R3
-
-    MOV R1 R6     
-    OUT R6
-
-    MOVI 1 R7
-    ADD R7 R1       
-
-    JMP R10
-
-end:
-    HALT
-
+    JAL  R12
+    POP  R4
+    POP  R3
+    OUT  R1
+    MOVI 1      R5
+    ADD  R5     R1
+    JMP  R8
 
 gcd:
     PUSH R30
-    MOV R31 R30      
+    MOV  R31    R30
+    LDLO -2     R3
+    LDLO -1     R4
+    MOV  R4     R5
+    JNZ  R5     R13
+    MOV  R3     R1
+    JMP  R14
 
-    LDLO -1 R4       
-    LDLO -2 R3       
-
-    
-    MOV R4 R6
-    JNZ R6 R14   
-
-    
-    MOV R3 R1        
-    JMP R15
-
-recurse:
-    
-    MOV R3 R5
-mod_loop:
-    MOV R5 R6       
-    SUB R4 R6        
-    JNZ R6 R12
-    JMP R22
-
-mod_continue:
-    MOV R6 R5       
-    JMP R20
-mod_done:
-
-    PUSH R4          
-    PUSH R5
-    JAL R13
-    POP R5
-    POP R4
+rec:
+    MOV  R3     R5
+    IDIV R4     R5
+    MUL  R4     R5
+    MOV  R3     R6
+    SUB  R5     R6
+    PUSH R4
+    PUSH R6
+    JAL  R12
+    POP  R6
+    POP  R4
+    JMP  R14
 
 return:
-    MOV R30 R31
-    POP R30
+    MOV  R30    R31
+    POP  R30
     RET
-    
+
+end:
+    HALT
